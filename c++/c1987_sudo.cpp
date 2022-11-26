cb4b0e47392ec7d5b6d0d7e4252ff60bc7c21438
jungyuha <yooha9621@gmail.com>
2022-11-26 17:19:28 +0900

    백준 1987번

--- c++/c1987_sudo.cpp
+++ c++/c1987_sudo.cpp
@@ -14,6 +14,7 @@ int vis[20][20]
 2. 수도코드 : dfs
 (메인)시작 정점 : qy,qx => (0,0)
     ap[mp1[0][0]-'A'] = 1
+    vis[0][0]=1;
     함수A(0,0,1)
 
 (함수A) : 시작 정점
@@ -21,7 +22,7 @@ int vis[20][20]
     (순회) 시작 정점의 상하좌우
         int nqy = qy+dy
         int nqx = qx+dx
-        if nqx,nqy 범위 초과 || ap[mp1[nqy][nqx]-'A'] > 0 
+        if nqx,nqy 범위 초과 || ap[mp1[nqy][nqx]-'A'] > 0  || vis 방문 체크
             continue
         ap[mp1[nqy][nqx]-'A'] = 1
         vis[nqy][nqx] == 1
@@ -29,4 +30,17 @@ int vis[20][20]
         ap[mp1[nqy][nqx]-'A'] = 0
         vis[nqy][nqx] == 0
     if(endYn) max비교
+
+3. 출력 테스트
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            cout << mp1[i][j];
+        }
+        cout << "\n";
+    }
+
+3 6
+HFDFFB
+AJHGDH
+DGAGEH
 */
