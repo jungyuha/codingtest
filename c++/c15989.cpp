fb2237bee71fd12f4fcd6bde5030fe1e36523c6d
jungyuha <yooha9621@gmail.com>
2023-01-14 16:08:12 +0900

    백준 15989번 코드

---
+++ c++/c15989.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int t,p,n,mp[10000][4],res[10000];
+int main() {
+
+    mp[0][1] =1; mp[0][2] =1; mp[0][3] =1;
+    for(int i=1;i<=10000;i++){
+        for(int j=1;j<=3;j++){
+            if(i-j>=0){
+                for(int jj=1;jj<=j;jj++){
+                    mp[i][j]+=mp[i-j][jj]; 
+                    res[i]+=mp[i-j][jj];
+                    if(i==j) jj=j+1;
+                }
+                // (1)
+  
+            }
+        }
+    }
+
+    cin >> t;
+    while(t){
+        cin >> p;
+        // (2)
+
+        t--;
+    }
+
+    return 0;
+}
