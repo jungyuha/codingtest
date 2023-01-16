19c61a0246dd5656c1728280def4458bed181171
jungyuha <yooha9621@gmail.com>
2023-01-16 12:00:16 +0900

    백준 2098(외판원 순회) 코드

--- c++/c2098.cpp
+++ c++/c2098.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 const int INF=987654321;
-int mp[16][16];
+int mp[16][16],dp[16][16];
 int n;
 int go(int here,int vis){
-    cout << "here : "<<here<<endl;
+    // cout << "here : "<<here<<endl;
     if(vis==(1<<n)-1){
         // if(mp[here][0]!=0 )cout << "end! ";
         // cout << endl;
         return (mp[here][0]!=0) ? mp[here][0] : INF ;
     }
+    if(dp[here][vis]!= -1) return dp[here][vis];
+    int res = INF;
     for(int i=0;i<n;i++){
-        cout << "("<<here<<")"<<i<<endl;
         if(vis&(1<<i)) continue;
         if(mp[here][i]==0) continue; 
-        go(i,vis|(1<<i));
+        res=min(res,go(i,vis|(1<<i))+mp[here][i]);
     }
+    dp[here][vis]=res;
+    return res;
 }
 int main(){
 
@@ -26,7 +29,7 @@ int main(){
             cin >> mp[i][j];
         }
     }
-
-    go(0,1);
+    memset(dp,-1,sizeof(dp));
+    cout << go(0,1) << endl;
     return 0;
 }
