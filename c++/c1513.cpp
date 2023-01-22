e123f47c86c4ab17fb3a3efa00fe3d41113823c7
jungyuha <yooha9621@gmail.com>
2023-01-22 23:54:49 +0900

    백준 1513번 코드

--- c++/c1513.cpp
+++ c++/c1513.cpp
@@ -1,35 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = -987654321;
 const int dx[] = {1,0};
 const int dy[] = {0,1};
-int n,m,c,t1,t2,mp[50],dp[5050][50];
-// int go(int idx){
-//     // (1) 
-//     if(prc == m){ return mp[idx][0];} // (2)
-//     if(prc > m) {  return 0;} //(3)
-//     if(dp[prc]!=-1) return dp[prc];
-//     int res=INF;
-//     for(int i=0;i<n;i++){
-//         res=max(res,go(prc+mp[i][1],i));
-//     }
-//     res+=mp[idx][0];
-//     dp[prc]=res;
-//     return res;
-// }
-int main(){
-        memset(dp,-1,sizeof(dp));
-        scanf("%d %d %d",&n,&m,&c);
+int x,y,c,t1,t2,mp[50][50],dp[50][50][50][50];
+int go(int cy,int cx,int prev,int cnt){
+    //cout << "go ["<<cy<<"]["<<cx <<"],prev:"<<prev<<",cnt:"<<cnt<<endl;
+    int m_res=0;
 
-        for(int i=0;i<c;i++){
-            scanf("%d %d",&t1,&t2);
-            mp[i]=t1*100+t2;
+    if(cnt < 0) return 0;
+    if(cy>y || cx>x) return 0;
+    if(dp[cy][cx][cnt][prev]!=-1) return dp[cy][cx][cnt][prev];
+    if(cy==y && cx ==x){
+        if(cnt ==1 && mp[cy][cx] >= prev){
+            // (1) 
+            return 1;
         }
-        int ret=INF;
-        for(int i=0;i<c;i++){
-            go(mp[i]));
+        else if(cnt == 0 && mp[cy][cx] == 0){
+            // (2)
+            return 1;
         }
-        cout <<ret<<endl;
+        else{
+            // (3)
+            return 0;
+        }
+    }
+
+    if(mp[cy][cx] > 0 && mp[cy][cx]>= prev){
+        m_res=go(cy+dy[0],cx+dx[0],mp[cy][cx]+1,cnt-1)+go(cy+dy[1],cx+dx[1],mp[cy][cx]+1,cnt-1);
+    }
+    else if(mp[cy][cx] == 0){
+        m_res = go(cy+dy[0],cx+dx[0],prev,cnt)+go(cy+dy[1],cx+dx[1],prev,cnt);
+    }
+
+    // (4)
+    dp[cy][cx][cnt][prev] = m_res;
+    return m_res;
+}
+int main(){
+    memset(dp,-1,sizeof(dp));
+    scanf("%d %d %d",&x,&y,&c); // x : 가로 , y : 세로
+    x--; y--;
+
+    for(int i=1;i<=c;i++){
+        scanf("%d %d",&t1,&t2); // 열, 행
+        mp[t2-1][t1-1]=i;
+    }
+
+    for(int i =0;i<=c;i++){
+        int ret = go(0,0,1,i);  // 행 , 열 , 오락실 순서 , 방문할 오락실 수       
+        cout <<ret<<" ";
+    }
+
     return 0;
 }
