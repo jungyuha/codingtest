2116801659906d231078fc9c367cf9d320bb87f4
jungyuha <yooha9621@gmail.com>
2023-03-02 23:50:48 +0900

    1514번 코드

--- c++/c1514_3.cpp
+++ c++/c1514_3.cpp
@@ -6,7 +6,8 @@ using namespace std;
 464
 */
 const int INF = 987654321;
-int n,pwd[110],cur[110],dp[110][10][10][10];
+int n,pwd[110],cur[110],dp[110][10][10][10][2];
+
 void init(){
     cin >> n;
     
@@ -20,7 +21,7 @@ void init(){
 }
 int make_num(int d,int ds,int c){
     // d : 방향(1:순/-1:역) , ds : 이동 거리
-    int result= c+(ds)*d;
+    int result= c+(ds*d);
     if(result > 9){
         result%=10;
     }
@@ -29,41 +30,28 @@ int make_num(int d,int ds,int c){
     }
     return result;
 }
-int go(int r,int c1,int c2,int c3,int k){
-    int &res = dp[r][c1][c2][c3];
-    int n1=c1,n2=c2,n3=c3;
-    if(r >= n) {return k;} // 뒤로 넘겼다는 건 == 바로 앞의 수가 pwd와 일치했다는 뜻
+int go(int r,int c1,int c2,int c3,int m,int k){
+    int &res = dp[r][c1][c2][c3][m];
+    if(r == n) {return 0;} // 뒤로 넘겼다는 건 == 바로 앞의 수가 pwd와 일치했다는 뜻
     if(res!=-1) { return res;}
     //if(k==15) { return 0;}
-    //cout << "r :" <<r <<","<<c1<<c2<<c3<<","<<pwd[r];
+    int m_=1;
+    if(m==1){m_=-1;}
     res=INF;
-if(k==1) cout << "r!! :" <<r <<",k:"<<k<<","<<c1<<c2<<c3<<",pwd[r]:"<<pwd[r]<<endl;
-    if(c1==pwd[r]) {r++; n1 = c2; n2=c3;n3=cur[r+2];} 
-    // 첫번째 자리
+    //if(k==2) {cout << "r!! :" <<r <<",k:"<<k<<","<<c1<<c2<<c3<<",pwd[r]:"<<pwd[r]<<endl; return 0;}
+    if(make_num(1,c1,cur[r])==pwd[r]) {return res=min(go(r+1,c2,c3,0,0,k+1),go(r+1,c2,c3,0,1,k+1));} 
+    
     for(int i=1;i<=3;i++){
-        res = min(res,go(r,make_num(1,i,n1),n2,n3,k+1));
+        
+        res = min(res,go(r,make_num(m_,i,c1),c2,c3,m_,k+1)+1);
+        res = min(res,go(r,make_num(m_,i,c1),make_num(m_,i,c2),c3,m_,k+1)+1);
+        res = min(res,go(r,make_num(m_,i,c1),make_num(m_,i,c2),make_num(m_,i,c3),m_,k+1)+1);
     }
-    for(int i=1;i<=3;i++){
-        res = min(res,go(r,make_num(-1,i,n1),n2,n3,k+1));
-    }
-    // 첫번째 + 두번째 자리
-    for(int i=1;i<=3;i++){
-        res = min(res,go(r,make_num(1,i,n1),make_num(1,i,n2),n3,k+1));
-    }
-    for(int i=1;i<=3;i++){
-        res = min(res,go(r,make_num(-1,i,n1),make_num(-1,i,n2),n3,k+1));
-    }
-    // 첫번째 + 두번째 +세번째 자리
-    for(int i=1;i<=3;i++){
-        res = min(res,go(r,make_num(1,i,n1),make_num(1,i,n2),make_num(1,i,n3),k+1));
-    }
-    for(int i=1;i<=3;i++){
-        res = min(res,go(r,make_num(-1,i,n1),make_num(-1,i,n2),make_num(-1,i,n3),k+1));
-    }
+    
     return res;
 }
 int main(){
     init();
-    cout << go(0,cur[0],cur[1],cur[2],0) << endl;
+    cout << min(go(0,0,0,0,1,0),go(0,0,0,0,1,0))<<endl;
     return 0;
 }
