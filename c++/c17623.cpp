22ac620de9f288482fee92421b423c772a470aea
jungyuha <yooha9621@gmail.com>
2023-03-06 21:45:23 +0900

    17623번 중간저장

--- c++/c17623.cpp
+++ c++/c17623.cpp
@@ -1,63 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int res_1=0;
+int res_1=0,pt=0;
+const int INF=-1;
 string st1;
+map<string,string> mp1;
 vector<pair<int,string>> stack_;
-int val_(int strt){
-    string curr="";
-    //vector<string> stack_;
-    int res_=0;
-    for(int i=strt;i<st1.length();i++){
-        //cout << st1[i] << "";
-        curr=st1[i];
-        if(curr=="(" || curr=="{" || curr=="[") {stack_.push_back({i,curr});}
-        else if(curr == ")") {
-            if(stack_.back().second !="("){cout << "불완전"; res_=-1;}
-            else if(stack_.back().first==i-1){
-                stack_.pop_back();
-                res_++;
-            }
-            else{
-                stack_.pop_back();
-                res_*=2;
-            }
-        }
-        else if(curr == "}") {
-            if(stack_.back().second !="{"){cout << "불완전"; res_=-1;}
-            else if(stack_.back().first==i-1){
-                stack_.pop_back();
-                res_+=2;
-            }
-            else{
-                stack_.pop_back();
-                res_*=3;
-            }
-        }
-        else if(curr == "]") {
-            if(stack_.back().second !="["){cout << "불완전"; res_=-1;}
-            else if(stack_.back().first==i-1){
-                stack_.pop_back();
-                res_+=3;
-                cout << res_ << endl;
-            }
-            else{
-                stack_.pop_back();
-                res_*=5;
-            }
-        }
-        // else if(curr=="(" || curr=="[" || curr=="{") {cout << "4 \n";res_+=val_(i,k+1);} //depth ++;
-        // 
-        // else if(stk == "{" && curr=="}") {cout << "6 \n";res_*=3; return res_;}
-        // else if(stk == "[" && curr=="]") {cout << "7 \n";res_*=5; return res_;}
-        
-        //prev=st1[i];
+int num_(string st,int gubun){
+    if(gubun==0){
+        if(st==")"){return 1;}
+        else if(st=="}"){return 2;}
+        else if(st=="]"){return 3;}
     }
-    cout << endl;
-    return res_;
+    else if(gubun==1){
+        if(st==")"){return 2;}
+        else if(st=="}"){return 3;}
+        else if(st=="]"){return 5;}
+    }
 }
+int val_(int strt){
+    pt=strt;
+    if(strt==st1.length()-1) return INF;
+    string st_,end_,curr;
+    st_=st1[strt];end_=mp1[st_];
+    int res_=0; 
+    for(int i=strt+1;i<st1.length();i++){
+        curr=st1[i];
+        if(curr=="(" || curr=="{" || curr =="["){
+            res_+=val_(i);
+            i=pt;
+        }
+        else if(curr==end_){
+            if(i==strt+1){ res_+=num_(end_,0); }
+            else{ res_*=num_(end_,1);}
+            pt = i;
+            return res_;
+        }
+    }
+    pt = st1.length();
+    return INF;
+}
+
 int main(){
-    st1="[][][]{()}";
-    cout << val_(0);
+    mp1["("]=")";mp1["{"]="}";mp1["["]="]";
+    //st1="[][][]{()}";
+    st1="[[[]]]";
+    //cout << val_(0);
+    pt=0;
+    int result =0;
+    while(pt<st1.length()){
+        result += val_(pt);
+        pt++;
+    }
+    cout << result <<endl;
     return 0;
 }
