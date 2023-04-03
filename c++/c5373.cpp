9534903bd5c6e3f65f30539acca0cc9543a7fbfe
jungyuha <yooha9621@gmail.com>
2023-04-03 20:54:13 +0900

    5373번 중간저장

--- c++/c5373.cpp
+++ c++/c5373.cpp
@@ -10,17 +10,17 @@ int num[6][12]={
     {27,24,21,12,15,18,36,33,30,9,6,3},
     {19,22,25,1,4,7,28,31,34,16,13,10}
     };
-int num[6][8]={
+int num2[6][8]={
     {1,2,3,6,9,8,7,4}, 
     {12,11,10,13,16,17,18,15},
     {19,20,21,24,27,26,25,22},
     {34,35,36,33,30,29,28,31},
-    {1,2,3,6,9,8,7,4},
-    {1,2,3,6,9,8,7,4}
+    {37,38,39,42,45,44,43,40},
+    {48,47,46,49,52,53,54,51}
     };
-int color_val[55];
+int color[55];
 map<char,int> ss;
-map<char,int> color;
+map<int,char> color_val;
 void init(){
     ss['F']=0;
     ss['B']=1;
@@ -29,12 +29,12 @@ void init(){
     ss['R']=4;
     ss['L']=5;
 
-    color['R']=0;
-    color['O']=1;
-    color['W']=2;
-    color['Y']=3;
-    color['B']=4;
-    color['G']=5;
+    color_val[0]='R';
+    color_val[1]='O';
+    color_val[2]='W';
+    color_val[3]='Y';
+    color_val[4]='B';
+    color_val[5]='G';
 
     int k=1;
     for (int i = 0; i < 6; i++) {
@@ -47,28 +47,41 @@ void init(){
     }
 }
 void rotate_(int front,char rl){
-    int ll;
+    int ll,ll2;
     if(rl=='+'){
-        ll = 3;
+        ll = 3; ll2 = 6;
     }
     else if(rl =='-'){
-        ll = 9;
+        ll = 9; ll2 = 2;
     }
     //cout << "before :::"<<front<<"\n";
-    vector<pair<int,int>> temp;
+    vector<pair<int,int>> temp; vector<pair<int,int>> temp2;
     for(int i=0;i<12;i++){
         int next_idx = (i+ll)%12;
         temp.push_back({num[front][next_idx],color[num[front][i]]});
     }
+    for(int i=0;i<8;i++){
+        int next_idx = (i+ll2)%8;
+        cout <<"!("<< num2[front][i] <<","<<color[num2[front][i]]<<","<<next_idx<<") ";
+        temp2.push_back({num2[front][i],color[num2[front][next_idx]]});
+    }
+    cout << endl;
     for(pair<int,int> el : temp){
         int a = el.first; int b = el.second;
         //cout <<"("<< a <<","<<b<<") ";
         color[a]=b;
     }
-    // cout << "after ::: \n";
-    // for(int el:num[front]){
-    //     cout <<"("<< el <<","<<color[el]<<") ";
-    // }
+    int k=0;
+    for(pair<int,int> el : temp2){
+        int a = el.first; int b = el.second;
+        //cout <<"("<< a <<","<<b<<") ";
+        color[a]=b;
+    }
+    cout << front << "after ::: \n";
+    for(int el:num2[front]){
+        cout <<"("<< el <<","<<color[el]<<") ";
+    }
+    cout << endl;
     for(int i=19;i<=27;i++){
         cout <<"("<< i <<","<<color[i]<<") ";
     }
@@ -93,6 +106,12 @@ int main() {
     cout << "result :: "<< endl;
     for(int i=19;i<=27;i++){
         cout <<"("<< i <<","<<color[i]<<") ";
+    }
+    int k=1;
+    for(int i=19;i<=27;i++){
+        cout <<color_val[color[i]];
+        if(k%3==0) cout << endl;
+        k++;
     }
     return 0;
 }
