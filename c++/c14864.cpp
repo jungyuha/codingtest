9a00c29866ded93db463e535b66d2d2dc68e5d15
jungyuha <yooha9621@gmail.com>
2023-04-06 01:58:03 +0900

    14864 중간저장

--- c++/c14864.cpp
+++ c++/c14864.cpp
@@ -13,21 +13,31 @@ void init(){
         int a,b;
         cin >> a >> b;
         vc1.push_back({a,b});
-        mp1[a]++;
+        mp1[a-1]++;
     }
 }
 int main(){
     init();
+    cout <<  "111 \n";
     for(int i=0;i<n;i++){
+        //mp1[i]
         int k =0,j=0;
-        while(1){
+        while(k<10){
+            cout <<  "k :: "<<k<<",j ::"<<j<<",mp1[j] ::"<<mp1[j]<<",mp2[j] ::"<<mp2[j]<<endl;
             if(mp2[j]==-1) {j++; continue;}
-            if(k==mp1[j]){
-                mp1[j]=-1;
-                res[i]=mp2[]
+            if(k==mp1[i]){
+                res_[i]=mp2[j];
+                mp2[j]=-1;
+                break;
             }
             j++;k++;
         }  
+        cout <<  "222 \n";
     }
+    
+    for(int i=0;i<n;i++){
+        cout << res_[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
