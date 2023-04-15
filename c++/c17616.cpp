ffced3f13a34bb6dfde5cc6e0acf8f05c3b9643f
jungyuha <yooha9621@gmail.com>
2023-04-15 15:59:30 +0900

    17616번 코드

--- c++/c17616.cpp
+++ c++/c17616.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 int n,m,x,a,b;
 vector<int> front_[100010],back_[100010];
-int res1[100010],res2[100010];
+int vis[100010];
 void init(){
     cin >> n >> m >> x;
-    fill(res1,res1+100010,1);fill(res2,res2+100010,n);
 
     for(int i=0;i<m;i++){
         cin >> a >> b ;
@@ -15,9 +14,8 @@ void init(){
     }
     queue<int> q1 , q2;
     for(int i=1;i<=n;i++){
-        int f_vis[100010],b_vis[100010];
-        for(int el : front_[i]){ q1.push(el); f_vis[el]=1;}
-        for(int el : back_[i]){ q2.push(el); b_vis[el]=1;}
+        for(int el : front_[i]){ q1.push(el); }
+        for(int el : back_[i]){ q2.push(el); }
         while(!q1.empty()){
             int pop_=q1.front(); q1.pop();
             for(int el : front_[pop_]){
@@ -25,16 +23,40 @@ void init(){
                 q1.push(el);
             }
         }
-    }
-    for(int i=1;i<=n;i++){
-        cout<<"i::"<<i <<"=>  ";
-        for(int el : front_[i]){
-            cout << el << " ";
+        while(!q2.empty()){
+            int pop_=q2.front(); q2.pop();
+            for(int el : back_[pop_]){
+                back_[i].push_back(el);
+                q2.push(el);
+            }
         }
-        cout << endl ;
     }
+    // for(int i=1;i<=n;i++){
+    //     cout<<"i::"<<i <<"=>  ";
+    //     for(int el : front_[i]){
+    //         cout << el << " ";
+    //     }
+    //     cout << endl ;
+    // }
+    // cout <<  " backend____ \n";
+    // for(int i=1;i<=n;i++){
+    //     cout<<"i::"<<i <<"=>  ";
+    //     for(int el : back_[i]){
+    //         cout << el << " ";
+    //     }
+    //     cout << endl ;
+    // }   
 }
 int main(){
     init();
+    int ff = 1; int bb = n;
+    for(int el : front_[x]){
+        if(vis[el]==0) { ff++; vis[el]=1;} 
+    }
+    fill(vis,vis+100010,0);
+    for(int el : back_[x]){
+        if(vis[el]==0) { bb--; vis[el]=1;} 
+    }
+    cout << ff << " " << bb << endl;
     return 0;
 }
